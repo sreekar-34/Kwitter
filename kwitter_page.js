cbf9559b62bf018770dd8791e534d25ec04b424d
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2022-11-11 17:57:20 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -21,7 +21,15 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+u_name = message_data['name'];
+message = message_data['message'];
+like = message_data['like'];
+name_with_tag="<h4>" + namee +"<img class='user-tick' src='tick.png'></h4>"
+message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
+like_button="<button class='btn btn-warning' id="+firebase_message_id+" value=" +like+" onclick='updatelike(this.id)'>"
+span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>like:"+like+"</span></button><hr>";
+row=name_with_tag + message_with_tag + like_button + span_with_tag;
+document.getElementById("output").innerHTML += row;
 //End code
       } });  }); }
 getData();
